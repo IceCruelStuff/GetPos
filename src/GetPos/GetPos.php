477260c8b9904351d5eea64f4ca2c86a541e6fc0
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-01-18 22:26:34 -0800

    Create GetPos.php

---
+++ src/GetPos/GetPos.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace GetPos;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as c;
+
+class GPS extends PluginBase {
+
+	public function onEnable(){
+	$this->getLogger()->info("GetPos enabled.");
+		return true;
+	}
+	
+
+	public function onLoad(){
+		$this->getLogger()->info("GetPos loaded");
+	}
+	
+
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+		switch($command->getName()){
+			case "getpos":
+				if($sender instanceof Player){
+					$playerX = $sender->getX();
+                	$playerY = $sender->getY();
+                	$playerZ = $sender->getZ();
+
+                	$outX=round($playerX,1);
+                	$outY=round($playerY,1);
+                	$outZ=round($playerZ,1);
+
+                	$playerLevel = $sender->getLevel()->getName();
+
+                	$sender->sendMessage(c::YELLOW."X:".c::RESET.c::AQUA . $outX . c::RESET.c::YELLOW." Y:".c::RESET.c::AQUA . $outY . c::RESET.c::YELLOW." Z:".c::RESET.c::AQUA . $outZ . c::RESET.c::YELLOW." Level: ".c::RESET.c::AQUA . $playerLevel);
+					return true;
+				}
+
+				else{
+					$sender->sendMessage("This command only works in-game.");
+            }
+		}
+	}    
+
+	
+    public function onDisable(){
+        $this->getLogger()->info("GetPos disabled.");
+        return true;
+	}
+}
