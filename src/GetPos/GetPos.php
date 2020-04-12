7622c6c661407bd3c4a6f7fe0df5301b09d99d6d
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-04-12 12:18:15 -0700

    small fix

--- src/GetPos/GetPos.php
+++ src/GetPos/GetPos.php
@@ -10,42 +10,34 @@ use pocketmine\utils\TextFormat as c;
 
 class GetPos extends PluginBase {
 
-	public function onEnable() : void{
-	$this->getLogger()->info("GetPos enabled.");
-	}
-	
+    public function onEnable() : void {
+        $this->getLogger()->info("GetPos enabled.");
+    }
 
-	public function onLoad() : void{
-		$this->getLogger()->info("GetPos loaded");
-	}
-	
+    public function onLoad() : void {
+        $this->getLogger()->info("GetPos loaded");
+    }
 
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-		switch($command->getName()){
-			case "getpos":
-				if($sender instanceof Player){
-					$playerX = $sender->getX();
-                	$playerY = $sender->getY();
-                	$playerZ = $sender->getZ();
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
+        switch($command->getName()){
+            case "getpos":
+                if($sender instanceof Player){
+                    $playerX = $sender->getX();
+                    $playerY = $sender->getY();
+                    $playerZ = $sender->getZ();
+                    $outX=round($playerX,1);
+                    $outY=round($playerY,1);
+                    $outZ=round($playerZ,1);
+                    $playerLevel = $sender->getLevel()->getName();
+                    $sender->sendMessage(c::YELLOW."X:".c::RESET.c::AQUA . $outX . c::RESET.c::YELLOW." Y:".c::RESET.c::AQUA . $outY . c::RESET.c::YELLOW." Z:".c::RESET.c::AQUA . $outZ . c::RESET.c::YELLOW." Level: ".c::RESET.c::AQUA . $playerLevel);
+                    return true;
+                } else {
+                    $sender->sendMessage("This command only works in-game.");
+                }
+        }
+    }    
 
-                	$outX=round($playerX,1);
-                	$outY=round($playerY,1);
-                	$outZ=round($playerZ,1);
-
-                	$playerLevel = $sender->getLevel()->getName();
-
-                	$sender->sendMessage(c::YELLOW."X:".c::RESET.c::AQUA . $outX . c::RESET.c::YELLOW." Y:".c::RESET.c::AQUA . $outY . c::RESET.c::YELLOW." Z:".c::RESET.c::AQUA . $outZ . c::RESET.c::YELLOW." Level: ".c::RESET.c::AQUA . $playerLevel);
-					return true;
-				}
-
-				else{
-					$sender->sendMessage("This command only works in-game.");
-            }
-		}
-	}    
-
-	
-    public function onDisable() : void{
+    public function onDisable() : void {
         $this->getLogger()->info("GetPos disabled.");
-	}
+    }
 }
